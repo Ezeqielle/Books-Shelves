804b83633bc8c93f2877a6dff4c7677ed01f2684
Ezeqielle <mathis.dimascio@gmail.com>
2021-07-21 11:07:30 +0200

    prisma:: init prisma

---
+++ prisma/migrations/20210721085459_init_db/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE `Books` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `url` VARCHAR(191) NOT NULL,
+    `lang` VARCHAR(191) NOT NULL,
+    `chap` INTEGER NOT NULL DEFAULT 0,
+    `status` VARCHAR(191) NOT NULL,
+    `type` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
